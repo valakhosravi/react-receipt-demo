7c50c6af3066df1772141bea63a0ce534a4035bc
Vala Khosravi <valakhosravi0@gmail.com>
2023-05-20 00:15:53 +0430

    Main layout implemented

--- src/App.js
+++ src/App.js
@@ -1,12 +1,30 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { AppBar, Toolbar, Typography, Container, TextField, Button } from '@mui/material';
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      
+    <div>
+      <AppBar position="static">
+        <Toolbar>
+          <Typography variant="h6">React Reciept Demo</Typography>
+        </Toolbar>
+      </AppBar>
+      <Container maxWidth="sm" sx={{ marginTop: '2rem' }}>
+        <TextField
+          id="outlined-multiline-static"
+          label="Enter your text"
+          multiline
+          rows={4}
+          fullWidth
+          variant="outlined"
+          sx={{ marginBottom: '1rem' }}
+        />
+        <Button variant="contained" color="primary">
+          Submit
+        </Button>
+      </Container>
     </div>
   );
-}
+};
 
 export default App;
